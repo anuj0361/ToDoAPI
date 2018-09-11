4ac9f05df7b5978725fd6bc0e6bc609b32730ffe
unknown <anujpandey1630@gmail.com>
2018-09-12 00:19:17 +0530

    Authentication for API

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,72 @@
-var mongoose=require('mongoose');
+const mongoose=require('mongoose');
+const validator=require('validator');
+const jwt=require('jsonwebtoken');
+onst _=require('lodash');
 
-var User=mongoose.model('User', {
+var UserSchema= new mongoose.Schema({
   email: {
     type:String,
     required:true,
     trim:true,
-    minlength:1
-  }
+    minlength:1,
+    unique:true,
+    validate: {
+      validator:validator.isEmail,
+      message: 'Email not valid'
+    }
+  },
+  password: {
+    type:String,
+    required:true,
+    minlength:1,
+  },
+  tokens: [{
+    access:{
+      type:String,
+      required:true
+    },
+    token:{
+      type:String,
+      required:true
+    }
+  }]
 });
+
+UserSchema.methods.toJSON=function(){
+  var user=this;
+  var userObject=user.toObject();
+
+  return ._pick(userObject,['_id','email']);
+};
+
+UserSchema.methods.generateAuthToken= function () {
+  var user=this;
+  var access='auth';
+  var token=jwt.sign({_id:user._id.toHexString(), access},'abc123').toString();
+
+  user.tokens=user.tokens.concat([{access,token}]);
+  return user.save().then(()=>{
+    return token;
+  });
+}
+
+UserSchema.statics.findByToken=function(token){
+  var User=this;
+  var decoded;
+   try{
+     decoded=jwt.verify(token,'abc123');
+   } catch (e) {
+     return Promise.reject();
+     });
+   }
+
+   return User.findOne({
+     _id:decoded._id,
+     'tokens.token':token,
+     'tokens.access':'auth'
+   });
+};
+
+var User=mongoose.model('User', UserSchema);
 
 module.exports={User};
--- server/server.js
+++ server/server.js
@@ -6,9 +6,11 @@ const {ObjectID}=require('mongodb');
 const {mongoose}=require('./db/mongoose');
 const {Todo}=require('./models/todo');
 const {Todo}=require('./models/user');
+var {authenticate}=require('./middleware/authenticate');
 
 var app=express();
 app.use(bodyParser.json());
+
 app.post('/todos', (req,res)=>{
   var todo=new Todo({
     text:req.body.text
@@ -88,6 +90,24 @@ app.patch('/todos/:id', (req,res)=>{
   }).catch((e)=>{
     res.status(400).send();
   });
+});
+
+
+app.post('/users', (req,res)=>{
+  var body=_.pick(req.body,['email','password']);
+  var user=new User({body});
+
+  user.save().then(()=>{
+    return user.generateAuthToken();
+  }).then((token)=>{
+    res.header('x-auth',token).send(user);
+  }).catch((e)=>{
+    res.status(400).send(e);
+  });
+});
+
+app.get('/users/me',authenticate,(req,res)=>{
+  res.send(req.user);
 });
 
 app.listen(3000, ()=>{
