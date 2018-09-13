60f3dfdb2e326407e49bc283fd50af51aadb565a
unknown <anujpandey1630@gmail.com>
2018-09-13 23:17:03 +0530

    DELETE user or signout

--- server/models/user.js
+++ server/models/user.js
@@ -104,6 +104,14 @@ UserSchema.statics.findByCredentials=function(email,password){
     });
 };
 
+UserSchema.methods.removeToken=function(token) {
+  var user = this;
+  return user.update({
+    $pull:{
+      tokens:{token}
+    }
+  });
+};
 
 var User=mongoose.model('User', UserSchema);
 
